6f18030f97e0d89e46614f34cccc243a2ab00a65
Aearost <unconfigured@null.spigotmc.org>
2021-08-12 22:46:35 -0400

    Added validation to onRecipeEntryAddRecipeButtonClick()

--- src/main/java/com/aearost/recipebook/RecipeEntryController.java
+++ src/main/java/com/aearost/recipebook/RecipeEntryController.java
@@ -1,7 +1,17 @@
 package com.aearost.recipebook;
 
+import com.aearost.recipebook.objects.Cost;
+import com.aearost.recipebook.objects.Cuisine;
+import com.aearost.recipebook.objects.MealType;
+import com.aearost.recipebook.objects.ProteinType;
+import com.aearost.recipebook.objects.Recipe;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
@@ -42,13 +52,108 @@ public class RecipeEntryController {
     @FXML
     private void onRecipeEntryAddRecipeButtonClick() throws IOException {
         
+        String name = nameEntryTextField.getText();
+        String description = descriptionEntryTextArea.getText();
+        List<String> ingredients = new ArrayList<>();
+        ingredients.addAll(Arrays.asList(ingredientsEntryTextArea.getText().split("\n")));
         
-        System.out.println("Do the validation!");
+        List<String> steps = new ArrayList<>();
+        steps.addAll(Arrays.asList(stepsEntryTextArea.getText().split("\n")));
         
+        // Note that imageUrl is optional so it does not need to be validated
+        boolean isFieldsValidated = validateFields(name, description, ingredients, steps);
+        boolean isNumberFieldsValidated = validateNumberFields(prepTimeEntryTextField.getText(), cookTimeEntryTextField.getText());
+        boolean isEnumFieldsValidated = validateEnumFields();
+        
+        if (isFieldsValidated && isNumberFieldsValidated && isEnumFieldsValidated) {
+            // Initialize the number fields
+            int prepTime = Integer.parseInt(prepTimeEntryTextField.getText());
+            int cookTime = Integer.parseInt(cookTimeEntryTextField.getText());
+            
+            //Initialize the enum fields
+            MealType mealType = MealType.valueOf(mealTypeEntryComboBox.getSelectionModel().getSelectedItem().toString().toUpperCase());
+            Cuisine cuisine = Cuisine.valueOf(cuisineEntryComboBox.getSelectionModel().getSelectedItem().toString().toUpperCase());
+
+            Cost cost;
+            String costContent = costEntryComboBox.getSelectionModel().getSelectedItem().toString();
+            switch (costContent) {
+                case "Between $10 and $15":
+                    cost = Cost.LT15;
+                    break;
+                case "Between $15 and $20":
+                    cost = Cost.LT20;
+                    break;
+                case "Between $20 and $30":
+                    cost = Cost.LT30;
+                    break;
+                case "More than $30":
+                    cost = Cost.GT30;
+                    break;
+                default:
+                    cost = Cost.LT10;
+                    break;
+            }
+
+            ProteinType proteinType = ProteinType.valueOf(proteinTypeEntryComboBox.getSelectionModel().getSelectedItem().toString().toUpperCase());
+            
+            Recipe recipe = new Recipe(name, description, ingredients, steps, mealType, cuisine, cost, prepTime, cookTime, proteinType);
+            System.out.println(recipe.toString());
+        } else {
+            Alert invalidFieldsAlert = new Alert(AlertType.INFORMATION);
+            invalidFieldsAlert.setHeaderText("Hey!");
+            invalidFieldsAlert.setContentText("One or more fields is filled incorrectly.");
+            invalidFieldsAlert.show();
+        }
         
     }
     
-    private void initializeSearchCriteria() {
+    
+    private boolean validateFields(String name, String description, List<String> ingredients, List<String> steps) {
+        if (name.length() < 1) {
+            return false;
+        }
+        if (description.length() < 1) {
+            return false;
+        }
+        if (ingredients.size() < 1) {
+            return false;
+        }
+        if (steps.size() < 1) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    private boolean validateNumberFields(String prepTimeString, String cookTimeString) {
+        int prepTime;
+        int cookTime;
+        try {
+            prepTime = Integer.parseInt(prepTimeString);
+            cookTime = Integer.parseInt(cookTimeString);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        if (prepTime < 0 || cookTime < 0) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    private boolean validateEnumFields() {
+        try {
+            mealTypeEntryComboBox.getSelectionModel().getSelectedItem().toString();
+            cuisineEntryComboBox.getSelectionModel().getSelectedItem().toString();
+            costEntryComboBox.getSelectionModel().getSelectedItem().toString();
+            proteinTypeEntryComboBox.getSelectionModel().getSelectedItem().toString();
+            return true;
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+    
+    private void initializeFormCriteria() {
         mealTypeEntryComboBox.getItems().removeAll(mealTypeEntryComboBox.getItems());
         mealTypeEntryComboBox.getItems().addAll("Breakfast", "Lunch", "Supper", "Sauce", "Snack", "Dessert");
         
@@ -67,6 +172,7 @@ public class RecipeEntryController {
     
     @FXML
     public void initialize() {
-        initializeSearchCriteria();
+        initializeFormCriteria();
     }
+
 }
--- src/main/java/com/aearost/recipebook/objects/Recipe.java
+++ src/main/java/com/aearost/recipebook/objects/Recipe.java
@@ -10,30 +10,33 @@ public class Recipe {
     private List<String> steps;
     private MealType mealType;
     private Cuisine cuisine;
+    private Cost cost;
     private int prepTime;
     private int cookTime;
     private ProteinType proteinType;
     private String imageUrl;
 
-    public Recipe(String name, String description, List<String> ingredients, List<String> steps, MealType mealType, Cuisine cuisine, int prepTime, int cookTime, ProteinType proteinType) {
+    public Recipe(String name, String description, List<String> ingredients, List<String> steps, MealType mealType, Cuisine cuisine, Cost cost, int prepTime, int cookTime, ProteinType proteinType) {
         this.name = name;
         this.description = description;
         this.ingredients = ingredients;
         this.steps = steps;
         this.mealType = mealType;
         this.cuisine = cuisine;
+        this.cost = cost;
         this.prepTime = prepTime;
         this.cookTime = cookTime;
         this.proteinType = proteinType;
     }
     
-    public Recipe(String name, String description, List<String> ingredients, List<String> steps, MealType mealType, Cuisine cuisine, int prepTime, int cookTime, ProteinType proteinType, String imageUrl) {
+    public Recipe(String name, String description, List<String> ingredients, List<String> steps, MealType mealType, Cuisine cuisine, Cost cost, int prepTime, int cookTime, ProteinType proteinType, String imageUrl) {
         this.name = name;
         this.description = description;
         this.ingredients = ingredients;
         this.steps = steps;
         this.mealType = mealType;
         this.cuisine = cuisine;
+        this.cost = cost;
         this.prepTime = prepTime;
         this.cookTime = cookTime;
         this.proteinType = proteinType;
@@ -95,6 +98,14 @@ public class Recipe {
     public void setCuisine(Cuisine cuisine) {
         this.cuisine = cuisine;
     }
+    
+    public Cost getCost() {
+        return cost;
+    }
+    
+    public void setCost(Cost cost) {
+        this.cost = cost;
+    }
 
     public int getPrepTime() {
         return prepTime;
@@ -118,6 +129,43 @@ public class Recipe {
 
     public void setProteinType(ProteinType proteinType) {
         this.proteinType = proteinType;
+    }
+    
+    @Override
+    public String toString() {
+        String lineSeparator = "------------------------\n";
+        String nameLine = "Recipe Name: " + this.name + "\n";
+        String descriptionLine = "Description: " + this.description + "\n";
+        String ingredientsLine = "Ingredients: ";
+        for (int i = 0; i < this.ingredients.size(); i++) {
+            if (i == this.ingredients.size() - 1) {
+                ingredientsLine += this.ingredients.get(i);
+            } else {
+                ingredientsLine += this.ingredients.get(i) + "***";
+            }
+        }
+        ingredientsLine += "\n";
+        String stepsLine = "Steps: ";
+        for (int i = 0; i < this.steps.size(); i++) {
+            if (i == this.steps.size() - 1) {
+                stepsLine += this.steps.get(i);
+            } else {
+                stepsLine += this.steps.get(i) + "***";
+            }
+        }
+        stepsLine += "\n";
+        String mealTypeLine = "Meal Type: " + this.mealType.toString() + "\n";
+        String cuisineLine = "Cuisine: " + this.cuisine.toString() + "\n";
+        String costLine = "Cost: " + this.cost.toString() + "\n";
+        String prepTimeLine = "Prep Time: " + this.prepTime + "\n";
+        String cookTimeLine = "Cook Time: " + this.cookTime + "\n";
+        String proteinTypeLine = "Protein Type: " + this.proteinType.toString();
+        if (this.imageUrl != null) {
+            proteinTypeLine += "\n";
+            String imageUrlLine = "Image URL: " + this.imageUrl;
+            return lineSeparator + nameLine + descriptionLine + ingredientsLine + stepsLine + mealTypeLine + cuisineLine + costLine + prepTimeLine + cookTimeLine + proteinTypeLine + imageUrlLine;
+        }
+        return lineSeparator + nameLine + descriptionLine + ingredientsLine + stepsLine + mealTypeLine + cuisineLine + costLine + prepTimeLine + cookTimeLine + proteinTypeLine;
     }
 
 }
